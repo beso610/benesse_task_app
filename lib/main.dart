b8f1bb4fd8de0026b709b06fbc9cf591f285ee08
beso610 <ryo.isobe.610@gmail.com>
2022-07-02 21:42:44 +0900

    カレンダーページの作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:benesse_task_app/ui/home.dart';
 import 'package:flutter/material.dart';
-import 'package:benesse_task_app/ui/calender.dart';
+import 'package:benesse_task_app/ui/calendar.dart';
 
 //void main() {
 //  runApp(const ScaffoldExample());
 //}
 
 void main() => runApp( new MaterialApp(
-  home: Home(),
+  //home: Home(),
+  home: CalendarScreen(),
   routes: <String, WidgetBuilder>{
-    '/calender': (BuildContext context) => CalenderScreen(),
+    '/calendar': (BuildContext context) => CalendarScreen(),
   },
 ));
